cdca0e55a120128ea927d7e7a7b60d68c5d64ead
Alan <epvelod@hotmail.com>
2019-06-17 08:29:45 -0500

    cambios inst

--- components/ItemComponente.js
+++ components/ItemComponente.js
@@ -19,19 +19,51 @@ import { MonoText, Titulo, SubTitulo, Descripcion, Dato } from '../components/St
 export default class ItemComponente extends React.Component {
 
   render() {
-
     return (
-
-  					<TouchableNativeFeedback onPress={() => this.props.onPress()}>
-						<View style={{ flexDirection: 'row' }}>
-			        		<CheckBox></CheckBox>
-			        		<Descripcion style={{marginTop: 5}}>
-			        		{this.props.children}
-			        		</Descripcion>
-						</View>
-  					</TouchableNativeFeedback>
-
-
+    	<View style={{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 10
+      }}>
+      	<View style={{ 
+          flexDirection: 'row'}}>
+          		<CheckBox value={this.props.value}
+              onChange={()=>this.props.onChange()}></CheckBox>
+          		<Descripcion style={{marginTop: 5}}>
+          		{this.props.children}
+          		</Descripcion>
+      	</View>
+        <View style={{
+          width: 80,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'stretch',
+          }}>
+          <View style={{
+            width: 80,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center', 
+            }}>
+            <TouchableWithoutFeedback onPress={() => this.props.onPress()} underlayColor="white">
+              <View style={{
+                height: 36,
+                width: 36,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center', 
+                borderRadius: 4,
+                }}>
+                <Icon.FontAwesome
+                  name="ellipsis-h"
+                  size={22}
+                  color={Colors.grisOscuro}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </View>
+    	</View>
     );
   }
 }
--- screens/Instruccion.js
+++ screens/Instruccion.js
@@ -1,55 +1,58 @@
 import React from 'react';
 import {
-  Alert,
   Button,
-  Image,
   Platform,
   ScrollView,
-  StyleSheet,
   Text,
   View,
-  ActivityIndicator,
-  CheckBox,
-  TouchableNativeFeedback,
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
-import ApolloClient from "apollo-boost";
-import { ApolloProvider, Query } from "react-apollo";
-import { gql } from "apollo-boost";
 
 import { MonoText, Titulo, Descripcion } from '../components/StyledText';
-import Card from '../components/Card';
-import Item from '../components/Item';
 import BotonListo from '../components/BotonListo';
 import ItemComponente from '../components/ItemComponente';
 
-import { bookListQuery } from '../constants/Queries';
 import Colors from '../constants/Colors';
 
 export default class Instruccion extends React.Component {
   static navigationOptions = {
     header: null,
   };
+  state={
+    selecteds:[],
+  }
   constructor(props){
     super(props);
   }
-
+  _onChange(index){
+    console.log(index);
+    const selecteds = this.state.selecteds;
+    selecteds[index] = !selecteds[index];
+    console.log(selecteds);
+    this.setState({
+      ...this.state,
+      selecteds: selecteds,
+    });
+  }
   render() {
+    /*rescue information*/
     const { navigation } = this.props;
-
     const traza = navigation.getParam('traza', undefined);
     const respuestas = navigation.getParam('respuestas', undefined);
     const {componentes,instruccion} = navigation.getParam('data', {instruccion:'...',componentes:[]});
-    console.log(componentes);
-    const items = componentes.map(({id_componente, descripcion, fallas}, index) => 
-        <ItemComponente 
+    console.log(traza);
+    console.log(respuestas);
+    /*build itmes*/
+    const items = componentes.map(({id_componente, descripcion, fallas}, index) => {
+        return (<ItemComponente 
           key={index} 
-          onPress={() => this.props.navigation.navigate('Fallas', {fallas: fallas})}>
+          onPress={() => this.props.navigation.navigate('Fallas', {fallas: fallas})}
+          value={this.state.selecteds[index]}
+          onChange={()=>this._onChange(index)}>
           {descripcion}
-        </ItemComponente>
+        </ItemComponente>);
+        }
       );
-
+    /*view*/
     return (
     <View style={{
       flex: 1,}}>
@@ -70,7 +73,7 @@ export default class Instruccion extends React.Component {
             {instruccion}
           </Descripcion>
         </View>
-        <ScrollView style={{paddingLeft: 30}}>
+        <ScrollView style={{paddingLeft: 10}}>
           {items}
         </ScrollView>
         <View style={{marginLeft: 10, marginBottom: 20, alignItems: 'flex-end' }}>
