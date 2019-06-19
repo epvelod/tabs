6b2a7de263703b88a8d63b9a94f4989ea270e5a4
lopezjair <lopezjail@globalhitss.com>
2019-06-19 13:58:56 -0500

    'camara'

---
+++ components/BotonCamara.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  StyleSheet,
+  CheckBox,
+  Text,
+  View,
+  TouchableHighlight, 
+  TouchableOpacity, 
+  TouchableNativeFeedback, 
+  TouchableWithoutFeedback, 
+} from 'react-native';
+
+import { Icon } from 'expo';
+
+import Colors from '../constants/Colors';
+import { MonoText, Titulo, SubTitulo, Descripcion, Dato } from '../components/StyledText';
+
+
+export default class BotonCamara extends React.Component {
+
+  render() {
+
+    return (
+      <TouchableOpacity
+       style={{
+       borderWidth:1,
+       borderColor:Colors.verdeClaro,
+       alignItems:'center',
+       justifyContent:'center',
+       width:60,
+       height:60,
+       backgroundColor:'#66cdaa',
+       borderRadius:30,
+       }}
+
+       onPress={() => this.props.onPress()}
+       >
+       <Icon.FontAwesome
+        name="camera"
+        size={26}
+        color="#fff"
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
